32e4a7666dae742bceeaf807a3c83bf77dc35285
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-08-18 17:08:48 +0530

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default [
+
+];
