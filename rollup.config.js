60f20e77dfb4a6a4213b6544e899041032c08b8e
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-08-18 17:20:45 +0530

    Delete rollup.config.js

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
-import pkg from './package.json';
-
-export default [
-
-];
